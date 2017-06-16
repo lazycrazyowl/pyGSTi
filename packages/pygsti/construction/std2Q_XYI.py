3aa8e41153b02ae9be85842162224e4291d09f3a
papageorge <alexander@rigetti.com>
2017-06-16 11:16:36 -0700

    fixed formatting in std2Q

--- packages/pygsti/construction/std2Q_XYI.py
+++ packages/pygsti/construction/std2Q_XYI.py
@@ -2,6 +2,7 @@ from pygsti.construction import gatestringconstruction as _strc
 from pygsti.construction import gatesetconstruction as _setc
 import itertools
 
+
 def add_1Q_expts(expt1, expt2):
     all_expt = itertools.product(expt1, expt2)
     ret_expts = []
@@ -15,7 +16,7 @@ def add_1Q_expts(expt1, expt2):
 
     return ret_expts
 
-fid_1Q = [('Gi',), ('Gx',), ('Gy',), ('Gx','Gx'), ('Gx','Gx','Gx'), ('Gy','Gy','Gy')]
+fid_1Q = [('Gi',), ('Gx',), ('Gy',), ('Gx', 'Gx'), ('Gx', 'Gx', 'Gx'), ('Gy', 'Gy', 'Gy')]
 
 germs_1Q = [('Gx',), ('Gy',), ('Gi',), ('Gx', 'Gy'), ('Gx', 'Gy', 'Gi'), ('Gx', 'Gi', 'Gy'),
             ('Gx', 'Gi', 'Gi'), ('Gy', 'Gi', 'Gi'), ('Gx', 'Gx', 'Gi', 'Gy'),
@@ -25,9 +26,14 @@ prepStrs = effectStrs = _strc.gatestring_list(add_1Q_expts(fid_1Q, fid_1Q))
 
 germs = _strc.gatestring_list(add_1Q_expts(germs_1Q, germs_1Q))
 
-gs_target = _setc.build_gateset([4], [('Q0', 'Q1')],['Gix', 'Giy', 'Gxi', 'Gyi', 'Gii', 'Gxx', 'Gyy', 'Gxy', 'Gyx'],
-    ["I(Q0):X(pi/2,Q1)", "I(Q0):Y(pi/2,Q1)", "X(pi/2,Q0):I(Q1)", "Y(pi/2,Q0):I(Q1)", "I(Q0):I(Q1)", "X(pi/2,Q0):X(pi/2,Q1)", "Y(pi/2,Q0):Y(pi/2,Q1)", "X(pi/2,Q0):Y(pi/2,Q1)", "Y(pi/2,Q0):X(pi/2,Q1)"],
-    prepLabels=['rho0'], prepExpressions=["0"],
-    effectLabels=['E0', 'E1', 'E2'], effectExpressions=["0", "1", "2"],
-    spamdefs={'upup': ('rho0', 'E0'), 'updn': ('rho0', 'E1'),
-              'dnup': ('rho0', 'E2'), 'dndn': ('rho0', 'remainder')}, basis="pp")
+gs_target = _setc.build_gateset([4], [('Q0', 'Q1')],
+                                ['Gix', 'Giy', 'Gxi', 'Gyi', 'Gii', 'Gxx', 'Gyy', 'Gxy', 'Gyx'],
+                                ["I(Q0):X(pi/2,Q1)", "I(Q0):Y(pi/2,Q1)", "X(pi/2,Q0):I(Q1)",
+                                 "Y(pi/2,Q0):I(Q1)", "I(Q0):I(Q1)", "X(pi/2,Q0):X(pi/2,Q1)",
+                                 "Y(pi/2,Q0):Y(pi/2,Q1)", "X(pi/2,Q0):Y(pi/2,Q1)",
+                                 "Y(pi/2,Q0):X(pi/2,Q1)"],
+                                prepLabels=['rho0'], prepExpressions=["0"],
+                                effectLabels=['E0', 'E1', 'E2'], effectExpressions=["0", "1", "2"],
+                                spamdefs={'upup': ('rho0', 'E0'), 'updn': ('rho0', 'E1'),
+                                          'dnup': ('rho0', 'E2'), 'dndn': ('rho0', 'remainder')},
+                                basis="pp")
